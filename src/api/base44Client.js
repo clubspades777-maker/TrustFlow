ae1ef681a02d1425506f854745b216c65a454a9d
Sah Isha Dharmendra <isha.shah2312@gmail.com>
2026-09-16 23:05:03 +0530

    Fix Base44 client authentication

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,22 +1,7 @@
-// import { createClient } from '@base44/sdk';
-// import { appParams } from '@/lib/app-params';
-
-// const { appId, token, functionsVersion, appBaseUrl } = appParams;
-
-// export const base44 = createClient({
-//   appId,
-//   token,
-//   functionsVersion,
-//   serverUrl: '',
-//   appBaseUrl
-// });
 import { createClient } from '@base44/sdk';
-
 import { appParams } from '@/lib/app-params';
 
 export const base44 = createClient({
   appId: appParams.appId,
-  headers: {
-    Authorization: `Bearer ${appParams.token}`,
-  },
+  token: appParams.token,
 });
